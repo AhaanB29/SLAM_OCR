1fc9137c8569a8807bc2c7fcc1c632224f8f17d3
AhaanBanerjee <ahaanbanerjee11@gmail.com>
2024-07-11 15:12:29 -0400

    Text-detect

---
+++ build/CMakeFiles/OCR.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OCR.dir/func.cpp.o"
+  "OCR"
+  "OCR.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OCR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
